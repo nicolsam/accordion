6ace9c3f2f77ab2907b6724783a21cacea62778a
Nicolas Samuel <nicolsam@pop-os.localdomain>
2022-07-27 14:42:48 -0300

    fix: module import

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,12 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.ts",
+    input: "./src/index.ts",
     output: [
       {
         file: packageJson.main,
@@ -25,7 +24,6 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss()
     ],
   },
   {
